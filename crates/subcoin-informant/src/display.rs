14d4b80b084ac47da55c2cb1a126bcc57a0789b8
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2024-07-03 20:53:42 +0800

    Introduce subcoin-informant

---
+++ crates/subcoin-informant/src/display.rs
@@ -0,0 +1,222 @@
+// This file is part of Substrate.
+
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::{ClientInfoExt, OutputFormat};
+use ansi_term::Colour;
+use bitcoin::hashes::Hash;
+use bitcoin::BlockHash;
+use sp_runtime::traits::{Block as BlockT, CheckedDiv, NumberFor, Saturating, Zero};
+use std::fmt::{self, Display};
+use std::time::Instant;
+
+struct DisplayBlockHash(BlockHash);
+
+impl Display for DisplayBlockHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        const BYTES: usize = 32;
+        let bytes = self.0.as_byte_array();
+        for i in bytes[BYTES - 2..BYTES].iter().rev() {
+            write!(f, "{:02x}", i)?;
+        }
+        write!(f, "…")?;
+        for i in bytes[0..3].iter().rev() {
+            write!(f, "{:02x}", i)?;
+        }
+        Ok(())
+    }
+}
+
+/// State of the informant display system.
+///
+/// This is the system that handles the line that gets regularly printed and that looks something
+/// like:
+///
+/// > Syncing  5.4 bps, target=#531028 (4 peers), best: #90683 (0x4ca8…51b8),
+/// > finalized #360 (0x6f24…a38b), ⬇ 5.5kiB/s ⬆ 0.9kiB/s
+///
+/// # Usage
+///
+/// Call `InformantDisplay::new` to initialize the state, then regularly call `display` with the
+/// information to display.
+pub struct InformantDisplay<B: BlockT> {
+    /// Head of chain block number from the last time `display` has been called.
+    /// `None` if `display` has never been called.
+    last_number: Option<NumberFor<B>>,
+    /// The last time `display` or `new` has been called.
+    last_update: Instant,
+    /// The last seen total of bytes received.
+    last_total_bytes_inbound: u64,
+    /// The last seen total of bytes sent.
+    last_total_bytes_outbound: u64,
+    /// The format to print output in.
+    format: OutputFormat,
+}
+
+impl<B: BlockT> InformantDisplay<B> {
+    /// Builds a new informant display system.
+    pub fn new(format: OutputFormat) -> InformantDisplay<B> {
+        InformantDisplay {
+            last_number: None,
+            last_update: Instant::now(),
+            last_total_bytes_inbound: 0,
+            last_total_bytes_outbound: 0,
+            format,
+        }
+    }
+
+    /// Displays the informant by calling `info!`.
+    pub fn display(&mut self, info: ClientInfoExt<B>) {
+        let best_number = info.chain.best_number;
+        let best_hash = info.chain.best_hash;
+        let best_bitcoin_hash = DisplayBlockHash(info.best_bitcoin_hash);
+        let finalized_number = info.chain.finalized_number;
+        let finalized_bitcoin_hash = DisplayBlockHash(info.finalized_bitcoin_hash);
+        let _speed = speed::<B>(best_number, self.last_number, self.last_update);
+        // TODO: proper value
+        let num_connected_peers = 0;
+        let total_bytes_inbound = 0;
+        let total_bytes_outbound = 0;
+
+        let now = Instant::now();
+        let elapsed = (now - self.last_update).as_secs();
+        self.last_update = now;
+        self.last_number = Some(best_number);
+
+        let diff_bytes_inbound = total_bytes_inbound - self.last_total_bytes_inbound;
+        let diff_bytes_outbound = total_bytes_outbound - self.last_total_bytes_outbound;
+        let (avg_bytes_per_sec_inbound, avg_bytes_per_sec_outbound) = if elapsed > 0 {
+            self.last_total_bytes_inbound = total_bytes_inbound;
+            self.last_total_bytes_outbound = total_bytes_outbound;
+            (diff_bytes_inbound / elapsed, diff_bytes_outbound / elapsed)
+        } else {
+            (diff_bytes_inbound, diff_bytes_outbound)
+        };
+
+        /* TODO Proper status
+        let (level, status, target) = match net_status.sync_status {
+            SyncStatus::Idle => ("💤", "Idle".into(), "".into()),
+            SyncStatus::Downloading { target, .. } => (
+                "⚙️ ",
+                format!("Syncing{speed}"),
+                format!(", target=#{target}"),
+            ),
+            SyncStatus::Importing { target, .. } => (
+                "⚙️ ",
+                format!("Preparing{speed}"),
+                format!(", target=#{target}"),
+            ),
+        };
+        */
+
+        let (level, status, target) = ("💤", "Idle", "");
+
+        let finalized_hash = info.chain.finalized_hash;
+
+        tracing::info!(
+            target: "subcoin",
+            "{level} {}{target} ({} peers), best: #{} ({best_bitcoin_hash},{best_hash}), confirmed #{} ({finalized_bitcoin_hash},{finalized_hash}), {} {}",
+            self.format.print_with_color(Colour::White.bold(), status),
+            self.format.print_with_color(Colour::White.bold(), num_connected_peers),
+            self.format.print_with_color(Colour::White.bold(), best_number),
+            self.format.print_with_color(Colour::White.bold(), finalized_number),
+            self.format.print_with_color(Colour::Green, format!("⬇ {}", TransferRateFormat(avg_bytes_per_sec_inbound))),
+            self.format.print_with_color(Colour::Red, format!("⬆ {}", TransferRateFormat(avg_bytes_per_sec_outbound))),
+        )
+    }
+}
+
+/// Calculates `(best_number - last_number) / (now - last_update)` and returns a `String`
+/// representing the speed of import.
+fn speed<B: BlockT>(
+    best_number: NumberFor<B>,
+    last_number: Option<NumberFor<B>>,
+    last_update: Instant,
+) -> String {
+    // Number of milliseconds elapsed since last time.
+    let elapsed_ms = {
+        let elapsed = last_update.elapsed();
+        let since_last_millis = elapsed.as_secs() * 1000;
+        let since_last_subsec_millis = elapsed.subsec_millis() as u64;
+        since_last_millis + since_last_subsec_millis
+    };
+
+    // Number of blocks that have been imported since last time.
+    let diff = match last_number {
+        None => return String::new(),
+        Some(n) => best_number.saturating_sub(n),
+    };
+
+    if let Ok(diff) = TryInto::<u128>::try_into(diff) {
+        // If the number of blocks can be converted to a regular integer, then it's easy: just
+        // do the math and turn it into a `f64`.
+        let speed = diff
+            .saturating_mul(10_000)
+            .checked_div(u128::from(elapsed_ms))
+            .map_or(0.0, |s| s as f64)
+            / 10.0;
+        format!(" {:4.1} bps", speed)
+    } else {
+        // If the number of blocks can't be converted to a regular integer, then we need a more
+        // algebraic approach and we stay within the realm of integers.
+        let one_thousand = NumberFor::<B>::from(1_000u32);
+        let elapsed =
+            NumberFor::<B>::from(<u32 as TryFrom<_>>::try_from(elapsed_ms).unwrap_or(u32::MAX));
+
+        let speed = diff
+            .saturating_mul(one_thousand)
+            .checked_div(&elapsed)
+            .unwrap_or_else(Zero::zero);
+        format!(" {} bps", speed)
+    }
+}
+
+/// Contains a number of bytes per second. Implements `fmt::Display` and shows this number of bytes
+/// per second in a nice way.
+struct TransferRateFormat(u64);
+impl fmt::Display for TransferRateFormat {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Special case 0.
+        if self.0 == 0 {
+            return write!(f, "0");
+        }
+
+        // Under 0.1 kiB, display plain bytes.
+        if self.0 < 100 {
+            return write!(f, "{} B/s", self.0);
+        }
+
+        // Under 1.0 MiB/sec, display the value in kiB/sec.
+        if self.0 < 1024 * 1024 {
+            return write!(f, "{:.1}kiB/s", self.0 as f64 / 1024.0);
+        }
+
+        write!(f, "{:.1}MiB/s", self.0 as f64 / (1024.0 * 1024.0))
+    }
+}
+
+#[test]
+fn test_display_block_hash() {
+    let genesis_hash: BlockHash =
+        "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+            .parse()
+            .unwrap();
+    assert_eq!(
+        format!("{}", DisplayBlockHash(genesis_hash)),
+        "0000…8ce26f".to_string()
+    );
+}
---
+++ crates/subcoin-informant/src/lib.rs
@@ -0,0 +1,219 @@
+//! # Subcoin Informant
+//!
+//! This crate is a fork of `sc-informant` for displaying the Subcoin network sync progress.
+
+mod display;
+
+use ansi_term::{Colour, Style};
+use bitcoin::BlockHash;
+use futures::prelude::*;
+use futures_timer::Delay;
+use sc_client_api::{AuxStore, BlockchainEvents, ClientInfo, UsageProvider};
+use sp_blockchain::{HeaderBackend, HeaderMetadata};
+use sp_runtime::traits::{Block as BlockT, Header};
+use std::collections::VecDeque;
+use std::fmt::Display;
+use std::ops::Deref;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::time::Duration;
+use subcoin_primitives::BackendExt;
+use tracing::{debug, info, trace};
+
+/// Extended [`ClientInfo`].
+#[derive(Debug)]
+struct ClientInfoExt<Block: BlockT> {
+    info: ClientInfo<Block>,
+    best_bitcoin_hash: BlockHash,
+    finalized_bitcoin_hash: BlockHash,
+}
+
+impl<Block: BlockT> Deref for ClientInfoExt<Block> {
+    type Target = ClientInfo<Block>;
+    fn deref(&self) -> &Self::Target {
+        &self.info
+    }
+}
+
+/// Creates a stream that returns a new value every `duration`.
+fn interval(duration: Duration) -> impl Stream<Item = ()> + Unpin {
+    futures::stream::unfold((), move |_| Delay::new(duration).map(|_| Some(((), ())))).map(drop)
+}
+
+/// The format to print telemetry output in.
+#[derive(Clone, Debug)]
+pub struct OutputFormat {
+    /// Enable color output in logs.
+    ///
+    /// Is enabled by default.
+    pub enable_color: bool,
+}
+
+impl Default for OutputFormat {
+    fn default() -> Self {
+        Self { enable_color: true }
+    }
+}
+
+enum ColorOrStyle {
+    Color(Colour),
+    Style(Style),
+}
+
+impl From<Colour> for ColorOrStyle {
+    fn from(value: Colour) -> Self {
+        Self::Color(value)
+    }
+}
+
+impl From<Style> for ColorOrStyle {
+    fn from(value: Style) -> Self {
+        Self::Style(value)
+    }
+}
+
+impl ColorOrStyle {
+    fn paint(&self, data: String) -> impl Display {
+        match self {
+            Self::Color(c) => c.paint(data),
+            Self::Style(s) => s.paint(data),
+        }
+    }
+}
+
+impl OutputFormat {
+    /// Print with color if `self.enable_color == true`.
+    fn print_with_color(
+        &self,
+        color: impl Into<ColorOrStyle>,
+        data: impl ToString,
+    ) -> impl Display {
+        if self.enable_color {
+            color.into().paint(data.to_string()).to_string()
+        } else {
+            data.to_string()
+        }
+    }
+}
+
+/// Builds the informant and returns a `Future` that drives the informant.
+pub async fn build<B: BlockT, C>(client: Arc<C>, format: OutputFormat)
+where
+    C: UsageProvider<B> + HeaderMetadata<B> + BlockchainEvents<B> + HeaderBackend<B> + AuxStore,
+    <C as HeaderMetadata<B>>::Error: Display,
+{
+    let mut display = display::InformantDisplay::new(format.clone());
+
+    let net_client = client.clone();
+
+    let display_notifications = interval(Duration::from_millis(5000)).for_each({
+        move |_| {
+            let info = net_client.usage_info();
+            if let Some(ref usage) = info.usage {
+                trace!(target: "usage", "Usage statistics: {}", usage);
+            } else {
+                trace!(
+                    target: "usage",
+                    "Usage statistics not displayed as backend does not provide it",
+                )
+            }
+
+            let best_bitcoin_hash = net_client
+                .bitcoin_block_hash_for(info.chain.best_hash)
+                .expect("Best bitcoin hash must exist; qed");
+
+            let finalized_bitcoin_hash = net_client
+                .bitcoin_block_hash_for(info.chain.finalized_hash)
+                .expect("Finalized bitcoin hash must exist; qed");
+
+            let client_info_ext = ClientInfoExt {
+                info,
+                best_bitcoin_hash,
+                finalized_bitcoin_hash,
+            };
+
+            display.display(client_info_ext);
+            future::ready(())
+        }
+    });
+
+    // TODO: proper status
+    let is_major_syncing = Arc::new(true.into());
+
+    futures::select! {
+        () = display_notifications.fuse() => (),
+        () = display_block_import(client, format, is_major_syncing).fuse() => (),
+    };
+}
+
+fn display_block_import<B: BlockT, C>(
+    client: Arc<C>,
+    format: OutputFormat,
+    is_major_syncing: Arc<AtomicBool>,
+) -> impl Future<Output = ()>
+where
+    C: UsageProvider<B> + HeaderMetadata<B> + BlockchainEvents<B>,
+    <C as HeaderMetadata<B>>::Error: Display,
+{
+    let mut last_best = {
+        let info = client.usage_info();
+        Some((info.chain.best_number, info.chain.best_hash))
+    };
+
+    // Hashes of the last blocks we have seen at import.
+    let mut last_blocks = VecDeque::new();
+    let max_blocks_to_track = 100;
+
+    client.import_notification_stream().for_each(move |n| {
+        // detect and log reorganizations.
+        if let Some((ref last_num, ref last_hash)) = last_best {
+            if n.header.parent_hash() != last_hash && n.is_new_best {
+                let maybe_ancestor =
+                    sp_blockchain::lowest_common_ancestor(&*client, *last_hash, n.hash);
+
+                match maybe_ancestor {
+                    Ok(ref ancestor) if ancestor.hash != *last_hash => info!(
+                        "♻️  Reorg on #{},{} to #{},{}, common ancestor #{},{}",
+                        format.print_with_color(Colour::Red.bold(), last_num),
+                        last_hash,
+                        format.print_with_color(Colour::Green.bold(), n.header.number()),
+                        n.hash,
+                        format.print_with_color(Colour::White.bold(), ancestor.number),
+                        ancestor.hash,
+                    ),
+                    Ok(_) => {}
+                    Err(e) => debug!("Error computing tree route: {}", e),
+                }
+            }
+        }
+
+        if n.is_new_best {
+            last_best = Some((*n.header.number(), n.hash));
+        }
+
+        if is_major_syncing.load(Ordering::Relaxed) {
+            return future::ready(());
+        }
+
+        // If we already printed a message for a given block recently,
+        // we should not print it again.
+        if !last_blocks.contains(&n.hash) {
+            last_blocks.push_back(n.hash);
+
+            if last_blocks.len() > max_blocks_to_track {
+                last_blocks.pop_front();
+            }
+
+            let best_indicator = if n.is_new_best { "🏆" } else { "🆕" };
+            info!(
+                target: "subcoin",
+                "{best_indicator} Imported #{} ({} → {})",
+                format.print_with_color(Colour::White.bold(), n.header.number()),
+                n.header.parent_hash(),
+                n.hash,
+            );
+        }
+
+        future::ready(())
+    })
+}
