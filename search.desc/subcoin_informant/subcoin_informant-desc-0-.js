c26b7fc159a13071e03be4ec76f6e4dc3d244b2f
liuchengxu <xuliuchengxlc@gmail.com>
2024-08-16 16:21:04 +0000

    Deploying to gh-pages from @ subcoin-project/subcoin@88308a1801a46dd9a454675cbfe074d68843621f 🚀

--- search.desc/subcoin_informant/subcoin_informant-desc-0-.js
+++ search.desc/subcoin_informant/subcoin_informant-desc-0-.js
@@ -1,1 +1,1 @@
-searchState.loadedDescShard("subcoin_informant", 0, "Subcoin Informant\nThe format to print telemetry output in.\nBuilds the informant and returns a <code>Future</code> that drives the …\nEnable color output in logs.\nReturns the argument unchanged.\nGet a mutable reference to the inner from the outer.\nGet a reference to the inner from the outer.\nCalls <code>U::from(self)</code>.")
+searchState.loadedDescShard("subcoin_informant", 0, "Subcoin Informant\nBuilds the informant and returns a <code>Future</code> that drives the …")
