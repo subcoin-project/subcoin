158685f1302e4151ff57b5249f2a378ba85cc1ae
liuchengxu <xuliuchengxlc@gmail.com>
2024-08-19 22:06:24 +0000

    Deploying to gh-pages from @ subcoin-project/subcoin@f44d30da409c13202114c85f2d75a528a832a56d 🚀

--- trait.impl/sc_consensus/import_queue/trait.Verifier.js
+++ trait.impl/sc_consensus/import_queue/trait.Verifier.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"subcoin_service":[["impl&lt;Block: BlockT&gt; Verifier&lt;Block&gt; for <a class=\"struct\" href=\"subcoin_service/struct.ImportQueueVerifier.html\" title=\"struct subcoin_service::ImportQueueVerifier\">ImportQueueVerifier</a>"]]
+"subcoin_service":[["impl&lt;Block: BlockT&gt; Verifier&lt;Block&gt; for <a class=\"struct\" href=\"subcoin_service/struct.SubstrateImportQueueVerifier.html\" title=\"struct subcoin_service::SubstrateImportQueueVerifier\">SubstrateImportQueueVerifier</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
