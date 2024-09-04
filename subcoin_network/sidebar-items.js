15d7d9166ba3d8ace4cef4e6eb3b14841e6fae84
liuchengxu <xuliuchengxlc@gmail.com>
2024-09-04 12:22:02 +0000

    Deploying to gh-pages from @ subcoin-project/subcoin@5b101427393c0b5fadcb6c3374dcb231339195cd 🚀

--- subcoin_network/sidebar-items.js
+++ subcoin_network/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Error","PeerLatency","PeerSyncState","SendTransactionResult","SyncStatus","SyncStrategy"],"struct":["Network","NetworkHandle","NetworkStatus","Params","PeerSync"],"type":["Latency","PeerId"]};
+window.SIDEBAR_ITEMS = {"enum":["Error","PeerLatency","PeerSyncState","SendTransactionResult","SyncStatus","SyncStrategy"],"struct":["Config","Network","NetworkHandle","NetworkStatus","PeerSync"],"type":["Latency","PeerId"]};
