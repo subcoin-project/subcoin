80dbebda15329d307162e8f81f410f4f92a54c27
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2024-10-20 21:33:01 +0800

    Make substrate sync work

---
+++ crates/subcoin-state-fetcher/src/cli.rs
@@ -0,0 +1,150 @@
+use clap::Parser;
+use sc_cli::{CliConfiguration, NetworkParams, SharedParams};
+use std::path::PathBuf;
+use subcoin_service::ChainSpec;
+
+const BITCOIN_MAINNET_CHAIN_SPEC: &str =
+    include_str!("../../subcoin-node/res/chain-spec-raw-bitcoin-mainnet.json");
+
+/// Fake CLI for satisfying the Substrate CLI interface.
+///
+/// Primarily for creating a Substrate runner.
+#[derive(Debug)]
+pub struct SubstrateCli;
+
+impl sc_cli::SubstrateCli for SubstrateCli {
+    fn impl_name() -> String {
+        "Subcoin UTXO Set Download Tool".into()
+    }
+
+    fn impl_version() -> String {
+        "0.1.0".to_string()
+    }
+
+    fn description() -> String {
+        env!("CARGO_PKG_DESCRIPTION").into()
+    }
+
+    fn author() -> String {
+        env!("CARGO_PKG_AUTHORS").into()
+    }
+
+    fn support_url() -> String {
+        "https://github.com/subcoin-project/subcoin/issues/new".into()
+    }
+
+    fn copyright_start_year() -> i32 {
+        2024
+    }
+
+    fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
+        let chain_spec = match id {
+            "bitcoin-mainnet" => ChainSpec::from_json_bytes(BITCOIN_MAINNET_CHAIN_SPEC.as_bytes())?,
+            "bitcoin-testnet" | "bitcoin-signet" => {
+                unimplemented!("Bitcoin testnet and signet are unsupported")
+            }
+            path => ChainSpec::from_json_file(std::path::PathBuf::from(path))?,
+        };
+
+        Ok(Box::new(chain_spec))
+    }
+}
+
+/// Bitcoin chain type.
+// TODO: This clippy warning will be fixed once more chains are supported.
+#[allow(clippy::enum_variant_names)]
+#[derive(Clone, Copy, Default, Debug, clap::ValueEnum)]
+pub enum Chain {
+    /// Bitcoin mainnet.
+    #[default]
+    BitcoinMainnet,
+    /// Bitcoin testnet
+    BitcoinTestnet,
+    /// Bitcoin signet.
+    BitcoinSignet,
+}
+
+impl Chain {
+    /// Returns the value of `id` in `SubstrateCli::load_spec(id)`.
+    pub fn chain_spec_id(&self) -> String {
+        // Convert to kebab-case for consistency in CLI.
+        match self {
+            Self::BitcoinMainnet => "bitcoin-mainnet".to_string(),
+            Self::BitcoinTestnet => "bitcoin-testnet".to_string(),
+            Self::BitcoinSignet => "bitcoin-signet".to_string(),
+        }
+    }
+}
+
+/// Subcoin UTXO Set State Download Tool
+#[derive(Debug, Parser)]
+#[clap(version = "0.1.0")]
+pub struct App {
+    /// Specify the chain.
+    #[arg(long, value_name = "CHAIN", default_value = "bitcoin-mainnet")]
+    pub chain: Chain,
+
+    /// Specify custom base path.
+    #[arg(long, short = 'd', value_name = "PATH")]
+    pub base_path: Option<PathBuf>,
+
+    /// Sets a custom logging filter (syntax: `<target>=<level>`).
+    ///
+    /// Log levels (least to most verbose) are `error`, `warn`, `info`, `debug`, and `trace`.
+    ///
+    /// By default, all targets log `info`. The global log level can be set with `-l<level>`.
+    ///
+    /// Multiple `<target>=<level>` entries can be specified and separated by a comma.
+    ///
+    /// *Example*: `--log error,sync=debug,grandpa=warn`.
+    /// Sets Global log level to `error`, sets `sync` target to debug and grandpa target to `warn`.
+    #[arg(short = 'l', long, value_name = "LOG_PATTERN", num_args = 1..)]
+    pub log: Vec<String>,
+
+    #[allow(missing_docs)]
+    #[clap(flatten)]
+    pub network_params: NetworkParams,
+}
+
+pub struct Command {
+    shared_params: SharedParams,
+    network_params: NetworkParams,
+}
+
+impl Command {
+    pub fn new(app: App) -> Self {
+        let App {
+            log,
+            chain,
+            base_path,
+            network_params,
+        } = app;
+
+        let shared_params = SharedParams {
+            chain: Some(chain.chain_spec_id()),
+            dev: false,
+            base_path,
+            log,
+            detailed_log_output: true,
+            disable_log_color: false,
+            enable_log_reloading: false,
+            tracing_targets: None,
+            tracing_receiver: sc_cli::TracingReceiver::Log,
+        };
+
+        Self {
+            shared_params,
+            network_params,
+        }
+    }
+}
+
+impl CliConfiguration for Command {
+    fn shared_params(&self) -> &SharedParams {
+        &self.shared_params
+    }
+
+    fn network_params(&self) -> Option<&NetworkParams> {
+        Some(&self.network_params)
+    }
+}
--- crates/subcoin-state-fetcher/src/main.rs
+++ crates/subcoin-state-fetcher/src/main.rs
@@ -1,158 +1,30 @@
 mod cli;
 
+use self::cli::{App, Command};
 use clap::Parser;
-use sc_cli::{NetworkParams as SubstrateNetworkParams, SubstrateCli};
+use sc_cli::SubstrateCli;
 use sc_consensus::import_queue::BasicQueue;
 use sc_consensus_nakamoto::SubstrateImportQueueVerifier;
 use sc_network::config::NetworkBackendType;
-use sc_service::config::{
-    BlocksPruning, DatabaseSource, ExecutorConfiguration, KeystoreConfig, NetworkConfiguration,
-    OffchainWorkerConfig, RpcBatchRequestConfig, RpcConfiguration, WasmExecutionMethod,
-    WasmtimeInstantiationStrategy,
-};
-use sc_service::{BasePath, Configuration, Role, TaskManager};
+use sc_service::{Configuration, TaskManager};
 use sp_runtime::traits::Block as BlockT;
-use std::path::PathBuf;
 use std::sync::Arc;
 use subcoin_runtime::interface::OpaqueBlock as Block;
 use subcoin_runtime::RuntimeApi;
 use subcoin_service::{FullClient, GenesisBlockBuilder, TransactionAdapter};
 
-/// Subcoin UTXO Set State Download Tool
-#[derive(Debug, Parser)]
-#[clap(version = "0.1.0")]
-pub struct Cli {
-    /// Specify custom base path.
-    #[arg(long, short = 'd', value_name = "PATH")]
-    pub base_path: Option<PathBuf>,
-
-    #[allow(missing_docs)]
-    #[clap(flatten)]
-    pub network_params: SubstrateNetworkParams,
-}
-
 fn main() -> sc_cli::Result<()> {
-    let Cli {
-        base_path,
-        network_params,
-    } = Cli::parse();
-
-    let base_path = base_path
-        .map(BasePath::from)
-        .unwrap_or_else(|| BasePath::from_project("", "", "subcoin_state_sync_node"));
+    let command = Command::new(App::parse());
 
     let bitcoin_network = bitcoin::Network::Bitcoin;
 
-    let tokio_runtime = sc_cli::build_runtime()?;
-
-    // `capture` needs to be called in a tokio context.
-    // Also capture them as early as possible.
-    let signals = tokio_runtime.block_on(async { sc_cli::Signals::capture() })?;
-
-    let chain_spec = cli::SubstrateCli.load_spec("bitcoin-mainnet")?;
-    let config_dir = base_path.config_dir(chain_spec.id());
-    let net_config_dir = config_dir.join("network");
-    let client_id = cli::SubstrateCli::client_id();
-
-    let node_name = sc_cli::generate_node_name();
-    let node_key = network_params
-        .node_key_params
-        .node_key(&net_config_dir, Role::Full, false)?;
-    let network_config = network_params.network_config(
-        &chain_spec,
-        false,
-        false,
-        Some(net_config_dir),
-        &client_id,
-        &node_name,
-        node_key,
-        30333,
-    );
-
-    let tokio_handle = tokio_runtime.handle().clone();
-    let config = create_configuration(
-        tokio_handle.clone(),
-        bitcoin_network,
-        base_path,
-        network_config,
-    );
-
-    let runner = sc_cli::Runner::<cli::SubstrateCli>::new(config, tokio_runtime, signals)?;
-
-    // TODO: command.init()
-
-    runner
-        .run_node_until_exit(|config| async move { new_state_sync_client(bitcoin_network, config) })
+    cli::SubstrateCli
+        .create_runner(&command)?
+        .run_node_until_exit(|config| async move { start_node(bitcoin_network, config) })
         .map_err(Into::into)
 }
 
-fn create_configuration(
-    tokio_handle: tokio::runtime::Handle,
-    bitcoin_network: bitcoin::Network,
-    base_path: BasePath,
-    network_config: NetworkConfiguration,
-) -> Configuration {
-    let root = PathBuf::from("/tmp");
-    let chain_spec =
-        subcoin_service::chain_spec::config(bitcoin_network).expect("Failed to create chain spec");
-    Configuration {
-        impl_name: "subcoin-state-sync-node".to_string(),
-        impl_version: "0.1.0".to_string(),
-        role: Role::Full,
-        tokio_handle,
-        transaction_pool: Default::default(),
-        network: network_config,
-        keystore: KeystoreConfig::InMemory,
-        database: DatabaseSource::ParityDb {
-            path: root.join("db"),
-        },
-        trie_cache_maximum_size: None,
-        state_pruning: None,
-        blocks_pruning: BlocksPruning::KeepAll,
-        chain_spec: Box::new(chain_spec),
-        executor: ExecutorConfiguration {
-            wasm_method: WasmExecutionMethod::Compiled {
-                instantiation_strategy: WasmtimeInstantiationStrategy::PoolingCopyOnWrite,
-            },
-            max_runtime_instances: 8,
-            runtime_cache_size: 2,
-            default_heap_pages: None,
-        },
-        rpc: RpcConfiguration {
-            addr: None,
-            max_connections: Default::default(),
-            cors: None,
-            methods: Default::default(),
-            max_request_size: Default::default(),
-            max_response_size: Default::default(),
-            id_provider: Default::default(),
-            max_subs_per_conn: Default::default(),
-            port: 9944,
-            message_buffer_capacity: Default::default(),
-            batch_config: RpcBatchRequestConfig::Unlimited,
-            rate_limit: None,
-            rate_limit_whitelisted_ips: Default::default(),
-            rate_limit_trust_proxy_headers: Default::default(),
-        },
-        prometheus_config: None,
-        telemetry_endpoints: None,
-        offchain_worker: OffchainWorkerConfig {
-            enabled: true,
-            indexing_enabled: false,
-        },
-        force_authoring: false,
-        disable_grandpa: false,
-        dev_key_seed: None,
-        tracing_targets: None,
-        tracing_receiver: Default::default(),
-        announce_block: true,
-        data_path: base_path.path().into(),
-        base_path,
-        wasm_runtime_overrides: None,
-    }
-}
-
-fn new_state_sync_client(
+fn start_node(
     bitcoin_network: bitcoin::Network,
     mut config: Configuration,
 ) -> Result<TaskManager, sc_service::error::Error> {
