32353abdbb5b2e462cbffcbea3ac40980cc70c8c
liuchengxu <xuliuchengxlc@gmail.com>
2025-01-09 05:13:32 +0000

    Deploying to gh-pages from @ subcoin-project/subcoin@3f5071cadfeb2baa7ec6214f50c62a6e3578822b 🚀

--- src-files.js
+++ src-files.js
@@ -3,6 +3,7 @@ var srcIndex = new Map(JSON.parse('[\
 ["pallet_executive",["",[],["lib.rs"]]],\
 ["sc_consensus_nakamoto",["",[["verification",[],["header_verify.rs","tx_verify.rs"]]],["aux_schema.rs","block_import.rs","chain_params.rs","import_queue.rs","lib.rs","metrics.rs","verification.rs","verifier.rs"]]],\
 ["subcoin_crypto",["",[],["lib.rs","muhash.rs"]]],\
+["subcoin_indexer",["",[],["lib.rs","transaction_indexer.rs"]]],\
 ["subcoin_informant",["",[],["display.rs","lib.rs"]]],\
 ["subcoin_network",["",[["sync",[["strategy",[],["blocks_first.rs","headers_first.rs"]]],["block_downloader.rs","orphan_blocks_pool.rs","strategy.rs"]]],["address_book.rs","checkpoint.rs","lib.rs","metrics.rs","network_api.rs","network_processor.rs","peer_connection.rs","peer_manager.rs","peer_store.rs","sync.rs","transaction_manager.rs"]]],\
 ["subcoin_node",["",[["cli",[],["subcoin_params.rs"]],["commands",[["blockchain",[],["dump_txout_set.rs","get_txout_set_info.rs","parse_block.rs","parse_txout_set.rs"]]],["blockchain.rs","import_blocks.rs","run.rs","tools.rs"]]],["cli.rs","commands.rs","lib.rs","rpc.rs","substrate_cli.rs","utils.rs"]]],\
---
+++ trait.impl/subcoin_primitives/trait.TransactionIndex.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"subcoin_indexer":[["impl&lt;Client&gt; TransactionIndex for <a class=\"struct\" href=\"subcoin_indexer/struct.TransactionIndexProvider.html\" title=\"struct subcoin_indexer::TransactionIndexProvider\">TransactionIndexProvider</a>&lt;Client&gt;<div class=\"where\">where\n    Client: AuxStore,</div>"]],
+"subcoin_primitives":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
